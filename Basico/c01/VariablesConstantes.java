35ab7f67538b63cf68b587acfd4f509f72d623f8
Nicolas Piñera <nicolaspinera@gmail.com>
2025-06-20 19:09:45 -0300

    Actualizacion: VariablesConstantes

--- Basico/c01/VariablesConstantes.java
+++ Basico/c01/VariablesConstantes.java
@@ -1,7 +1,29 @@
 package c01;
 
 public class VariablesConstantes {
+    //Funcion Principal
     public static void main(String[] args) {
+        //Tipos de Variables
+        // Variable String o Texto
+        String nombre= "Nicolas"; //TipoDato NombreVariable = Valor
+        System.out.println(nombre);
+
+        nombre = "Beto"; //Cambio de valor
+        System.out.println(nombre);
         
+        //Variable entera
+        int edad = 22; 
+        System.out.println("Mi edad es: " + edad);
+
+        // var asigna el tipo de variable en la primera instanciacion
+        var email = "nicolas@gmail.com";
+        System.out.println(email);
+
+        
+
+        // Constante = final - modificador para variables constantes, no puede variar
+        // Las constanes se nombran en MAYUSCULAS 
+        final String EMAIL = "nicolaspinera@gmail.com";
+        System.out.println(EMAIL);
     }
 }
