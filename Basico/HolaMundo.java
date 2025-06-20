1d0681749452dc5c9302628bdb2056cc79eacb6e
Nicolas Piñera <nicolaspinera@gmail.com>
2025-06-20 18:12:24 -0300

    Creacion: Primer archivo Java - HolaMundo

---
+++ Basico/HolaMundo.java
@@ -0,0 +1,16 @@
+// Todo se encapsula dentro de una clase
+public class HolaMundo {
+
+    // El método main es el punto de entrada de la aplicación
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo"); //Mostrar por pantalla
+        // Comentario de una sola linea
+
+
+        /*Comentario
+        * de
+        * varias
+        * lineas
+        * */
+    }
+}
