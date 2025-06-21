0f6c52b727b2a9547eb209fbf231d9af5c6d2299
Nicolas Piñera <nicolaspinera@gmail.com>
2025-06-21 20:31:39 -0300

    Actualizacion: Estructuras de Datos - Arrays

--- Basico/c05_estructurasDatos/EstructurasDatos.java
+++ Basico/c05_estructurasDatos/EstructurasDatos.java
@@ -2,8 +2,25 @@ package c05_estructurasDatos;
 
 public class EstructurasDatos {
     public static void main(String[] args) {
-        
+
         // Estructura de datos
+        // Nos sirven para guardar mas de un dato
+
+        // Array
+
+        // Declaracion y creacion
+        int[] numero = new int[3]; // Array de enteros - Le establezco la longitud
+        System.out.println(numero); // Muestra la direccion de memoria
+        System.out.println(numero.length); // Longitud
+
+        String[] nombres = { "Brais", "Nico", "Juan" };
+        System.out.println(nombres[0]); // Acceso
+        System.out.println(nombres[1]);
+        System.out.println(nombres[2]);
+
+        // Modificacion
+
+        //1:22:00
     }
-    
+
 }
